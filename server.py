8739a5de719ec0afd082a1140499bb369995d998
Garrett Webb <garrettwebb97@gmail.com>
2021-04-16 11:01:51 -0700

    implemented GET

--- server.py
+++ server.py
@@ -22,83 +22,58 @@ from sys import argv
 kvstore = {}
 
 class requestHandler(http.server.BaseHTTPRequestHandler):
-    kvstore = {}
     def _set_headers(self, response_code):
         self.send_response(response_code)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
     def do_GET(self):
-        print("GET: unimplelmented")
-        self._set_headers(response_code=500)
-        return
-    
-    # this is here as legacy in case we need to reuse any of the code
-    # just collapse the function in VScode
-    def do_POST(self):
-        if str(self.path) == '/ping':
-            #if there is something after the ping, this will not trigger
-            self._set_headers(response_code=405)
-            response = "Method Not Allowed"
-            response = bytes(response, 'utf-8')
+        if str(self.path).startswith("/key-value-store/"):
+            keystr = str(self.path).split("/key-value-store/",1)[1]
+            if(len(keystr) > 0 and len(keystr) < 50):
+                if keystr in kvstore:
+                    self._set_headers(response_code=200)
+                    response = bytes(json.dumps({"doesExist" : True, "message" : "Retrieved successfully", "value" : kvstore[keystr]}), 'utf-8')
+                else:
+                    self._set_headers(response_code=404)
+                    response = bytes(json.dumps({"doesExist" : False, "error" : "Key does not exist", "message" : "Error in GET"}), 'utf-8')
+            elif (len(keystr) > 50):
+                self._set_headers(response_code=400)
+                response = bytes(json.dumps({'error' : "Key is too long", 'message' : "Error in GET"}), 'utf-8')
+            elif(len(keystr) == 0):
+                self._set_headers(response_code=400)
+                response = bytes(json.dumps({'error' : "Key not specified", 'message' : "Error in GET"}), 'utf-8')
             self.wfile.write(response)
-        
-        elif str(self.path).startswith("/ping"):
-            #if starts with ping, but has something after, send 200
-            self._set_headers(response_code=200)
-            #get the substring after the /ping/
-            response_str = str(self.path).split("/ping/",1)[1]
-            response = bytes(json.dumps({'message' : "I'm alive, " + response_str + "!!"}), 'utf-8')
-            self.wfile.write(response)
-        
-        elif str(self.path) == '/echo':
-            # not a ping, but an echo
-            self._set_headers(response_code=400)
-            response = bytes("Bad Request", 'utf-8')
-            self.wfile.write(response)
-        
-        elif str(self.path).startswith("/echo?"):
-            # echo with a message
-            self._set_headers(response_code=200)
-            #get the message from the echo
-            response_str = str(self.path).split("/echo?msg=",1)[1]
-            response = bytes(json.dumps({'message' : response_str}), 'utf-8')
-            self.wfile.write(response)
-
         else:
             #default 500 code to clean up loose ends
             self._set_headers(response_code=500)
-
         return
 
     def do_PUT(self):
         if str(self.path).startswith("/key-value-store/"):
-            #get the substring after the /ping/
-            response_str = str(self.path).split("/key-value-store/",1)[1]
-            if(len(response_str) > 0 and len(response_str) < 50):
+            keystr = str(self.path).split("/key-value-store/",1)[1]
+            if(len(keystr) > 0 and len(keystr) < 50):
                 self.data_string = self.rfile.read(int(self.headers['Content-Length']))
                 data = json.loads(self.data_string)
                 if "value" not in data:
                     self._set_headers(response_code=400)
                     response = bytes(json.dumps({'error' : "Value is missing", 'message' : "Error in PUT"}), 'utf-8')
-                elif response_str in kvstore:
-                    kvstore[response_str] = data["value"]
+                elif keystr in kvstore:
+                    kvstore[keystr] = data["value"]
                     self._set_headers(response_code=200)
                     response = bytes(json.dumps({'message' : "Updated successfully", 'replaced' :True}), 'utf-8')
                 else:
-                    kvstore[response_str] = data["value"]
+                    kvstore[keystr] = data["value"]
                     self._set_headers(response_code=201)
                     response = bytes(json.dumps({'message' : "Added successfully", 'replaced' :False}), 'utf-8')
-            elif (len(response_str) > 50):
+            elif (len(keystr) > 50):
                 self._set_headers(response_code=400)
                 response = bytes(json.dumps({'error' : "Key is too long", 'message' : "Error in PUT"}), 'utf-8')
             
             self.wfile.write(response)
-        
         else:
             #default 500 code to clean up loose ends
             self._set_headers(response_code=500)
-
         return
     
     def do_DELETE(self):
@@ -110,7 +85,6 @@ def run(server_class=http.server.HTTPServer, handler_class=requestHandler, addr=
     # this function initializes and runs the server on the class defined above
     server_address = (addr, port)
     httpd = server_class(server_address, handler_class)
-    kvstore = {}
     try:
         print(f"Starting HTTP server on {addr}:{port}")
         httpd.serve_forever()
